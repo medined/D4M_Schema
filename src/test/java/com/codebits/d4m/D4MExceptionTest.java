a0d7584ca66efc628429770f7822ab651d51ac28
David Medinets <david.medinets@gmail.com>
2014-05-01 13:54:33 -0400

    Provide specific exception.

---
+++ src/test/java/com/codebits/d4m/D4MExceptionTest.java
@@ -0,0 +1,31 @@
+package com.codebits.d4m;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class D4MExceptionTest {
+    
+    private final static Throwable throwable = new Throwable();
+        
+    @Test
+    public void testConstructor() {
+        assertNotNull(new D4MException());
+    }
+    
+    @Test
+    public void testConstructor_with_string() {
+        assertNotNull(new D4MException("message"));
+    }
+    
+    @Test
+    public void testConstructor_with_string_and_throwable() {
+        assertNotNull(new D4MException("message", throwable));
+    }
+    
+    @Test
+    public void testConstructor_with_throwable() {
+        assertNotNull(new D4MException(throwable));
+    }
+    
+}
