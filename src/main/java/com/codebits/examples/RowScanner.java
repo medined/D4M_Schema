552b90003eb6f7d7f17688af984a542ca70489c0
David Medinets <david.medinets@gmail.com>
2014-04-30 23:46:39 -0400

    Provide example of code to read accumulo table.

---
+++ src/main/java/com/codebits/examples/RowScanner.java
@@ -0,0 +1,55 @@
+package com.codebits.examples;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Properties;
+import org.apache.accumulo.core.client.AccumuloException;
+import org.apache.accumulo.core.client.AccumuloSecurityException;
+import org.apache.accumulo.core.client.Connector;
+import org.apache.accumulo.core.client.Scanner;
+import org.apache.accumulo.core.client.TableExistsException;
+import org.apache.accumulo.core.client.TableNotFoundException;
+import org.apache.accumulo.core.client.ZooKeeperInstance;
+import org.apache.accumulo.core.data.Key;
+import org.apache.accumulo.core.data.Range;
+import org.apache.accumulo.core.data.Value;
+import org.apache.accumulo.core.security.Authorizations;
+import org.cbda.PropertyManager;
+
+/*
+ * Run 'mvn package' before running this class so the
+ * property file is coped to the target directory.
+*/
+
+public class RowScanner {
+
+    public static void main(String[] args) throws AccumuloException, AccumuloSecurityException, TableNotFoundException, TableExistsException, IOException {
+
+        PropertyManager propertyManager = new PropertyManager();
+        propertyManager.setPropertyFilename("cbda.properties");
+        Properties properties = propertyManager.load();
+
+        String instanceName = properties.getProperty("accumulo.instance.name");
+        String zooKeepers = properties.getProperty("accumulo.zookeeper.ensemble");
+        String user = properties.getProperty("accumulo.user");
+        byte[] pass = properties.getProperty("accumulo.password").getBytes();
+
+        String tableName = "word_to_index";
+        String rowId = "1001";
+
+        ZooKeeperInstance instance = new ZooKeeperInstance(instanceName, zooKeepers);
+        Connector connector = instance.getConnector(user, pass);
+
+        Scanner scan = connector.createScanner(tableName, new Authorizations());
+        scan.setRange(new Range(rowId, rowId));
+
+        Iterator<Map.Entry<Key, Value>> iterator = scan.iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<Key, Value> entry = iterator.next();
+            Key key = entry.getKey();
+            Value value = entry.getValue();
+            System.out.println(key + " ==> " + value);
+        }
+    }
+}
