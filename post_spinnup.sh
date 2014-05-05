5f7d92d5cd5d083c2ab31680b09f9ecc74c8d65c
David Medinets <david.medinets@gmail.com>
2014-05-05 18:01:55 -0400

    Move hadoop and octave setup into .bashrc file.

--- post_spinnup.sh
+++ post_spinnup.sh
@@ -9,12 +9,15 @@ vagrant ssh master -c "start-all.sh"
 
 vagrant ssh master -c "zkServer.sh start"
 
+vagrant ssh slave2 -c "cp /vagrant/octaverc /home/vagrant/.octaverc"
+
+vagrant ssh slave2 -c "echo 'Creating Hadoop directories .. ignore file exists error.' >> /home/vagrant/.bashrc"
+vagrant ssh slave2 -c "cat /vagrant/hadoop_setup.sh >> /home/vagrant/.bashrc"
+vagrant ssh slave2 -c "cat /vagrant/octave_setup.sh >> /home/vagrant/.bashrc"
+
 echo "Waiting 30 seconds for Hadoop to get started."
 sleep 30
 
 echo "Now connect to the master node to start accumulo:"
 echo "  vagrant ssh slave2"
 echo "  accumulo_home/bin/accumulo/bin/start-all.sh"
-echo "  /vagrant/hadoop_setup.sh"
-echo "  /vagrant/octave_setup.sh"
-echo "  cp /vagrant/octaverc .octaverc"
