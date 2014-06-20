078900dc1bc264e265db7f74db21d75430aa41de
David Medinets <david.medinets@gmail.com>
2014-06-20 12:55:50 -0400

    Rename RecordController

--- rest/src/main/java/com/codebits/d4m/rest/controller/RecordController.java
+++ rest/src/main/java/com/codebits/d4m/rest/controller/RecordController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class D4MRecordController {
+public class RecordController {
 
     @Autowired
     private AccumuloService accumuloService = null;
