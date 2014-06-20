1d31dac6afcd8cd75d4dd1a61ae9e86f2bf7c8cc
David Medinets <david.medinets@gmail.com>
2014-06-20 17:07:22 -0400

    Add hostName, hostIp, lamportClock, and wallClock to each response

--- rest/src/main/java/com/codebits/d4m/rest/model/D4MResponse.java
+++ rest/src/main/java/com/codebits/d4m/rest/model/D4MResponse.java
@@ -1,16 +1,47 @@
 package com.codebits.d4m.rest.model;
 
+import com.codebits.d4m.D4MException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Date;
+import java.util.concurrent.atomic.AtomicLong;
 import lombok.Getter;
 import lombok.Setter;
 
 public class D4MResponse {
 
+    @Getter
+    private static final AtomicLong lamportClockTracker = new AtomicLong();
+
+    @Getter
+    private String hostName = null;
+    
+    @Getter
+    private String hostIp = null;
+
     @Setter
     @Getter
     private String message = null;
-    
+
     @Setter
     @Getter
     private Throwable throwable = null;
+
+    @Getter
+    private final long wallClock = new Date().getTime();
+
+    public D4MResponse() {
+        lamportClockTracker.incrementAndGet();
+        try {
+            hostName = InetAddress.getLocalHost().getHostName();
+            hostIp = InetAddress.getByName(hostName).getHostAddress();
+        } catch (UnknownHostException ex) {
+            throw new D4MException("Unable to get hostname.");
+        }
+    }
+
+    public long getLamportClock() {
+        return lamportClockTracker.get();
+    }
     
 }
